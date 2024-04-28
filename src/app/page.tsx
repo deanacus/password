246ade5710fec33cc5611341525f8116f77b3411
Dean Harris <dean@harris.tc>
2024-04-28 19:35:58 +1000

    Add suspense boundary for useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,30 @@
 import { Button, RepoLink } from '@/components';
 import { useKeyUp } from '@/hooks/useKeyPress';
 import { usePassword } from '@/hooks/usePassword';
+import { Suspense } from 'react';
 
-const Page = () => {
+const Body = () => {
   const { password, generate, copyPassword } = usePassword();
   useKeyUp('Enter', copyPassword);
   useKeyUp('Space', generate);
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p className="text-4xl fnt-bold">{password}</p>
+      <div className="flex gap-4">
+        <Button onClick={copyPassword}>Copy to clipboard</Button>
+        <Button onClick={generate}>Gimme somethin' different</Button>
+      </div>
+    </div>
+  );
+};
 
+const Page = () => {
   return (
     <main className="flex flex-col items-center justify-center static min-h-screen">
-      <div className="flex flex-col items-center gap-6">
-        <p className="text-4xl fnt-bold">{password}</p>
-        <div className="flex gap-4">
-          <Button onClick={copyPassword}>Copy to clipboard</Button>
-          <Button onClick={generate}>Gimme somethin' different</Button>
-        </div>
-      </div>
+      <Suspense>
+        <Body />
+      </Suspense>
+
       <div className="flex flex-col items-center text-xs gap-4 justify-center absolute inset-x-0 bottom-0 pb-11">
         <p>Built with ❤ by Dean Harris</p>
         <p>
