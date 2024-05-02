c9531b83fc83f3016c89f7c34e8cae02a9d9a07f
Dean Harris <dean@harris.tc>
2024-05-02 17:52:49 +1000

    feat(password): update usePassword to use options context

--- src/hooks/usePassword.ts
+++ src/hooks/usePassword.ts
@@ -1,28 +1,37 @@
-import { generatePassword } from '@/utils';
-import { useSearchParams } from 'next/navigation';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { passwordGeneratorFactory } from '@/utils';
 import { useCopy } from './useCopy';
+import { useOptions } from './useOptions';
 
 export const usePassword = () => {
-  const [isClient, setIsClient] = useState(false);
-  const params = useSearchParams();
-  const length = parseInt(params.get('length') ?? '12');
-  const [password, setPassword] = useState(generatePassword(length));
+  const { lower, upper, numeric, special, length } = useOptions();
+  const [password, setPassword] = useState<string>('');
   const copy = useCopy();
 
+  const generatePassword = useMemo(() => {
+    return passwordGeneratorFactory({
+      lower,
+      upper,
+      numeric,
+      special,
+      length,
+    });
+  }, [lower, upper, numeric, special, length]);
+
   useEffect(() => {
-    setIsClient(true);
+    setPassword(generatePassword());
   }, []);
 
   const copyPassword = useCallback(async () => {
-    await copy(password);
+    await copy(password ?? '');
     setPassword('Copied');
     setTimeout(() => setPassword(password), 750);
   }, [password]);
 
   return {
-    password: isClient ? password : '',
-    generate: () => setPassword(generatePassword(length)),
+    password: password,
+    generate: () => setPassword(generatePassword()),
     copyPassword,
   };
 };
