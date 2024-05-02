b5369d6b62f86cccf2980435f531bf5cbc011175
Dean Harris <dean@harris.tc>
2024-05-02 17:55:22 +1000

    refactor: clean up useKeyPress file and clean up imports/exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useKeyPress } from './useKeyPress';
 export { useOptions } from './useOptions';
 export { usePassword } from './usePassword';
 export { useQueryParams } from './useQueryParams';
--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,30 +1,18 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 
-interface UseKeyPressOptions {
-  targetKey: KeyboardEvent['key'];
-}
-
-const hasWindow = (): boolean => {
-  return typeof window === 'object';
-};
-
-// code: KeyboardEvent['code'];
-// callback: () => void|Promise<void>
-
-type UseKeyUp = (
+type UseKeyPress = (
   code: KeyboardEvent['code'],
-  callback: () => void | Promise<void>,
-) => {};
-
-export const useKeyUp = (
-  targetCode: KeyboardEvent['code'],
   onKeyPress: () => void | Promise<void>,
-) => {
+) => void;
+
+export const useKeyPress: UseKeyPress = (code, onKeyPress) => {
   const handler = useCallback(
-    ({ code }: KeyboardEvent) => {
-      code === targetCode && onKeyPress();
+    (e: KeyboardEvent) => {
+      if (e.code === code) {
+        onKeyPress();
+      }
     },
-    [targetCode],
+    [code],
   );
 
   useEffect(() => {
@@ -35,43 +23,3 @@ export const useKeyUp = (
     };
   }, []);
 };
-
-export default function useKeyPress({
-  targetKey,
-}: UseKeyPressOptions): boolean {
-  const [keyPressed, setKeyPressed] = useState(false);
-
-  const downHandler = useCallback(
-    ({ key }: KeyboardEvent) => {
-      if (key === targetKey) {
-        setKeyPressed(true);
-      }
-    },
-    [targetKey],
-  );
-
-  const upHandler = useCallback(
-    ({ key }: KeyboardEvent) => {
-      if (key === targetKey) {
-        setKeyPressed(false);
-      }
-    },
-    [targetKey],
-  );
-
-  useEffect(() => {
-    if (!hasWindow()) {
-      return;
-    }
-
-    window.addEventListener('keydown', downHandler);
-    window.addEventListener('keyup', upHandler);
-
-    return () => {
-      window.removeEventListener('keydown', downHandler);
-      window.removeEventListener('keyup', upHandler);
-    };
-  }, [downHandler, upHandler]);
-
-  return keyPressed;
-}
